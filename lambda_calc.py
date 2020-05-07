5d41016c0a29e369eb7977a482efa31e08fef72b
Andrew Fichman <andrew.fichman@gmail.com>
2020-05-06 23:38:13 -0400

    split impl from test file, updated .gitignore

--- lambda_calc.py
+++ lambda_calc.py
@@ -1,16 +1,8 @@
-import argparse, sys
-
-counter = -1
-def fresh():
-    global counter
-    counter += 1
-    return "@%d" % counter
-
-def resetFresh():
-    global counter
-    counter = -1
+##########################################################################
+# Parsing/reduction for the Lambda Calculus
 
 ##########################################################################
+# Abstract base class for a Lambda Calculus Expression
 class LambdaExpr(object):
     def red(self):
         pass
@@ -95,49 +87,12 @@ def parse(data):
     # FixMe: implement!
     return App(Abs("x", Var("x")), Abs("x", Var("x")))
         
-def testReduction():
-    # Simple formulas
-    assert str(Var("x").red()) == "x"
-    assert str(Abs("x", Var("M")).red()) == "(Lx.M)"
-    assert str(App(Var("M"), Var("N")).red()) == "(M N)"
+counter = -1
+def fresh():
+    global counter
+    counter += 1
+    return "@%d" % counter
 
-    # Applications of variables
-    assert str(App(Var("x"), Var("y")).red()) == "(x y)"
-    assert str(App(Var("x"), Abs("x", Var("M"))).red()) == "(x (Lx.M))"
-    assert str(App(Var("x"), App(Var("M"), Var("N"))).red()) == "(x (M N))"
-
-    # Applications of basic abstractions
-    assert str(App(Abs("x", Var("x")), Var("x")).red()) == "x"
-    assert str(App(Abs("x", Var("x")), Abs("x", Var("x"))).red()) == "(Lx.x)"
-    assert str(App(Abs("x", Var("x")), App(Var("M"), Var("N"))).red()) == "(M N)"
-
-
-    # Application of abstraction with capture-avoiding substitution
-    assert str(App(Abs("x", Var("M")), Var("x")).red()) == "M"
-    assert str(App(Abs("x", Var("M")), Var("z")).red()) == "M"
-    assert str(App(Abs("x", Var("x")), Var("r")).red()) == "r"
-    assert str(App(Abs("x", Var("y")), Var("r")).red()) == "y"
-    assert str(App(Abs("x", App(Var("t"), Var("s"))), Var("r")).red()) == "(t s)"
-    assert str(App(Abs("x", App(Var("x"), Var("x"))), Var("r")).red()) == "(r r)"
-    assert str(App(Abs("x", Abs("x", Var("t"))), Var("r")).red()) == "(Lx.t)"
-    body = Abs("x", App(Var("x"), Var("y")))
-    abst = Abs("y", body)
-    assert str(App(abst, Var("x")).red()) == "(L@0.(@0 x))" # renaming example (alpha-conversion)
-    resetFresh()
-
-    # Application of applications
-    assert str(App(App(Var("M"), Var("N")), Var("x")).red()) == "((M N) x)"
-    assert str(App(App(Var("M"), Var("N")), Abs("x", Var("x"))).red()) == "((M N) (Lx.x))"
-    assert str(App(App(Var("M"), Var("N")), App(Var("A"), Var("B"))).red()) =="((M N) (A B))"
-
-def main():
-    testReduction()
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file", help="path to lambda calc file", type=str)
-    args = parser.parse_args()
-    with open(args.file) as f:
-        expr = parse(f.read())
-        print(expr.red())
-
-if __name__ == "__main__":
-    main()
+def resetFresh():
+    global counter
+    counter = -1
